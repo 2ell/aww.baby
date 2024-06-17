b5ad00451b486bb94e9ed17fafd0902ebd9b8e23
2ell <laurel@omg.lol>
2024-06-16 20:21:24 -0600

    ~ fix temp returns

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,10 @@
 module.exports = function(eleventyConfig) {  
 // Copy `src/style.css` to `_site/style.css`  
     eleventyConfig.addPassthroughCopy("src/style.css");
-    eleventyConfig.setTemplateFormats("html,liquid,njk");
     
     return {    
     // When a passthrough file is modified, rebuild the pages:
         passthroughFileCopy: true,
-    // Templates
-		templateFormats: ["html", "liquid", "njk"],
-        markdownTemplateEngine: 'njk',
-        dataTemplateEngine: 'njk',
-        htmlTemplateEngine: 'njk',
     // Set custom directories for input, output, includes, and data:
         dir: {      
             input: "src",      
