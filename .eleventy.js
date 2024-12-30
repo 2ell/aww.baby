81f7e1fd8a433b9550d307afeeb15c260199aa79
2ell <laurel@omg.lol>
2024-12-30 14:56:48 -0700

    + aww.baby svg headers

--- .eleventy.js
+++ .eleventy.js
@@ -27,6 +27,7 @@ export default function(eleventyConfig) {
 	// Copy any .jpg file to `_site`, via Glob pattern
 	// Keeps the same directory structure.
 	eleventyConfig.addPassthroughCopy("**/*.jpg");
+  eleventyConfig.addPassthroughCopy("**/*.svg");
 
   // Layout aliasing
   eleventyConfig.addLayoutAlias("post", "post.njk");
