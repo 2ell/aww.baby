34c26f38bbc403295803c4992075272bfca63391
Laurel <laurel@omg.lol>
2025-07-12 16:57:29 -0600

    chore: file cleanup

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,3 +1,4 @@
+import sass from 'eleventy-sass';
 import {getAllPosts, showInSitemap, tagList} from './src/_config/collections.js';
 import filters from './src/_config/filters.js';
 import markdownIt from 'markdown-it';
@@ -112,5 +113,36 @@ export default function(eleventyConfig) {
       });
   });
 
+eleventyConfig.addExtension("scss", {
+  outputFileExtension: "css",
+  compile: async function (inputContent, inputPath) {
+    // Skip files like _fileName.scss
+    let parsed = path.parse(inputPath);
+    if (parsed.name.startsWith("_")) {
+      return;
+    }
+
+    // Run file content through Sass
+    let result = sass.compileString(inputContent, {
+      loadPaths: [parsed.dir || "."],
+      sourceMap: false, // or true, your choice!
+    });
+
+    // Allow included files from @use or @import to
+    // trigger rebuilds when using --incremental
+    this.addDependencies(inputPath, result.loadedUrls);
+
+    return async () => {
+      return result.css;
+    };
+  },
+});
+
+  return  {
+    dir: {
+      input: "src",
+      output: "dist"
+    }
+  };
 
   }
